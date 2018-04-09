959a35d899bbb4c24ce7c9539c4aa432bc6aee6a
XiLi <1437565863@qq.com>
2018-04-09 14:12:20 -0400

    learn to code ch2

--- Python/ch3/Rock,Paper,Seissor.py
+++ Python/ch3/Rock,Paper,Seissor.py
@@ -1,5 +1,5 @@
 import random
-computer_choice = random.chose('rock','paper','scissors')
+computer_choice = random.choice(('rock','paper','scissors'))
 
 #if random_choice == 0:
 #	computer_choice = 'rock'
@@ -8,7 +8,22 @@ computer_choice = random.chose('rock','paper','scissors')
 #else:
 #	computer_choice = 'scissors'
 
-print('The computer chooses',computer_choice)
-
-user_choice = input('rock,paper or scissors? ')
-print('You chose',user_choice,'and the computer chose', computer_choice)
+#print('The computer chooses',computer_choice)
+user_choice = ''
+while user_choice != 'rock' and user_choice != 'paper' and user_choice != 'scissors':
+    user_choice = input('rock, paper or scissors? ')
+#print('You chose',user_choice,'and the computer chose', computer_choice)
+if user_choice == computer_choice:
+    winner = 'Tie'
+elif computer_choice == 'paper' and user_choice == 'rock':
+    winner = 'Computer'
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    winner = 'Computer'
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    winner = 'computer'
+else:
+    winner = 'User'
+if winner == 'Tie':
+    print('We both chose',computer_choice + ', play again.')
+else:
+    print(winner,'won. The computer chose',computer_choice + '.')
