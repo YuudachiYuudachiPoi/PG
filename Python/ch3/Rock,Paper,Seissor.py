fa3adb00473a646dc10794e7040e9e0f5ae06d33
XiLi <1437565863@qq.com>
2018-04-11 13:30:11 -0400

    编程课

--- Python/ch3/Rock,Paper,Seissor.py
+++ Python/ch3/Rock,Paper,Seissor.py
@@ -11,7 +11,7 @@ computer_choice = random.choice(('rock','paper','scissors'))
 #print('The computer chooses',computer_choice)
 user_choice = ''
 while user_choice != 'rock' and user_choice != 'paper' and user_choice != 'scissors':
-    user_choice = input('rock, paper or scissors? ')
+    user_choice = input('rock, paper or scissors? ').lower()
 #print('You chose',user_choice,'and the computer chose', computer_choice)
 if user_choice == computer_choice:
     winner = 'Tie'
