0499fcf292e37177b2068a0044d921a0b18bdc06
XiLi <1437565863@qq.com>
2018-04-18 09:51:04 -0400

    编程课 2018/4/17

---
+++ ISU3U/python/unit 2/as1e1.py
@@ -0,0 +1,19 @@
+print('Wlcome to my program! ')
+import random
+
+num = random.randint(-10,20)
+print(num)
+
+if num < 10:
+    print('Nice grade 1 number')
+    print('If the number is 7 that is a luck number. ')
+
+elif num == 10:
+    print("That's waht I want on my quit!?")
+
+else:
+    print('That is a big boy number. ')
+
+print('This line happends no matter what. ')
+
+print('Thanks to use my program! ')
---
+++ ISU3U/python/unit 2/test.py
@@ -0,0 +1,16 @@
+import random
+
+x=0
+
+ll = {}
+for n in range(-10,10):
+    ll[n] = 0
+
+for n in range(10000000):
+    num = random.randint(-10,10)
+    #print(num)
+    x+=num
+    if num in ll:
+        ll[num] += 1
+print(x)
+print(ll)
