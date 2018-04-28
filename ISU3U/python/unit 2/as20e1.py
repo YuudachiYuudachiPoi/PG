4b1ea1332dbec618857489eb0fe687f4a2819e6b
XiLi <1437565863@qq.com>
2018-04-28 18:51:55 -0400

    更改一些小东西

---
+++ ISU3U/python/unit 2/as20e1.py
@@ -0,0 +1,35 @@
+data = open()
+
+
+
+
+
+
+
+#---------------
+import tkinter as tk
+window = tk.Tk()
+
+window.title('My windows')
+window.geometry('1920x1080')
+
+help = tk.Label(window,
+    text='Press tap in your name.',
+    bg='white',
+    font=('Arial',12),
+    width=15,height=2
+    )
+
+search = tk.Botton(window,
+    text='search',
+    width=15,height=2,
+    command = search_people
+    )
+
+help.pack()
+search.pack()
+
+def search_people():
+
+
+window.mainloop()
