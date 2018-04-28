44ac6e86caba237b5141636462113f8d0c99c372
XiLi <1437565863@qq.com>
2018-04-28 18:57:40 -0400

    打开错文件，更改回来

--- Python/婚礼名单开发/名单处理小软件开发.py
+++ Python/婚礼名单开发/名单处理小软件开发.py
@@ -1,5 +1,36 @@
+data = open('people list.txt','r')
+
+
+
+
+
+
+
+
+#---------------
 import tkinter as tk
+window = tk.Tk()
+
+window.title('My windows')
+window.geometry('1920x1080')
+
+help = tk.Label(window,
+    text='Press tap in your name.',
+    bg='white',
+    font=('Arial',12),
+    width=15,height=2
+    )
+
+search = tk.Botton(window,
+    text='search',
+    width=15,height=2,
+    command = search_people
+    )
+
+help.pack()
+search.pack()
+
+def search_people():
 
 
-
-top.mainloop()
+window.mainloop()
