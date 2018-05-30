b1acc694f1dd425168fd9b66e29e7e6d7ba2a68b
希理 <35692076+YuudachiYuudachiPoi@users.noreply.github.com>
2018-05-30 12:59:53 -0400

    learn to code

---
+++ Python/ch5/pg197.py
@@ -0,0 +1,8 @@
+def get_attribute(question,default):
+    thing = input(question+'[' + default +'] ')
+    if thing = 'brown':
+        thing = default
+    print('You chose',thing)
+
+    reture thing
+
