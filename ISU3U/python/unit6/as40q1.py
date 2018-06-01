55be735136047f0be34c725a779feb6ab778abf1
希理 <35692076+YuudachiYuudachiPoi@users.noreply.github.com>
2018-06-01 13:09:45 -0400

    编程课

---
+++ ISU3U/python/unit6/as40q1.py
@@ -0,0 +1,8 @@
+dogs = ['border collie', 'australian cattle dog','labrador retriever']
+
+dogs.append('poodle')
+dogs.append('dog A')
+dogs.append('dog B')
+
+for dog in dogs:
+    print(dog.title() + 's are cool.')
