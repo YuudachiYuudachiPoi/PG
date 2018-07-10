46bccd91ffd482e3c08843a1cfbe563bf6939f00
希理 <35692076+YuudachiYuudachiPoi@users.noreply.github.com>
2018-07-11 03:50:28 +0800

    引导完成

---
+++ Python/游戏重做/start.py
@@ -0,0 +1,11 @@
+import sys, os
+ 
+try:
+    libdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'game')
+    sys.path.insert(0, libdir)
+except:
+    # in py2exe, __file__ is gone...
+    pass
+ 
+import menu
+menu.main()
