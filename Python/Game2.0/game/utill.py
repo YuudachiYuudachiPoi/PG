3f80a581d91380676b0c6d89139f2d046327beef
希理 <35692076+YuudachiYuudachiPoi@users.noreply.github.com>
2018-07-11 04:27:56 +0800

    中文支持

---
+++ Python/Game2.0/game/utill.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pygame,os
+from pygame.locals import *
+
+def init_pygame(screen_size=(1280,720)):
+    pygame.mixer.pre_init(44100,16,2,8192)
+    pygame.init()   
+    pygame.display.set_caption('希理的游戏2.0')
+    pygame.mouse.set_visible(False)
+    
+    return pygame.display.set_mode(screen_size,0,32)
+
+def make_font(message,background = None,color=(0,0,0),antialias = True):
+    pwd = os.path.realpath(__file__)
+    path_game = os.path.abspath(os.path.dirname(pwd)+os.path.sep+"..")
+    f = open(path_game+'\data\setting.txt','r')
+    font = pygame.font.Font(path_game+'\data\SIMSUN.TTC',40)
+    #font = pygame.font.Font('SIMSUN.TTC',40)
+    #font = pygame.font.SysFont('simsunnsimsun',40)
+    surface = font.render(message,antialias,color,background)
+
+    return surface
