f33b165a931d3eb5db74c0e0dc743c1d661601c6
希理 <1437565863@qq.com>
2019-02-04 12:37:51 -0500

    教同学

---
+++ Python/log.py
@@ -0,0 +1,7 @@
+n = []
+for k in range(100):
+    o = k/100
+    n.append(o)
+
+for na in n:
+    print((-2.0)^(na))
---
+++ 教傻逼/1.py
@@ -0,0 +1,20 @@
+a = 10
+#print(a)
+a = a*2
+#print(a)
+a -= 1 # a = a-1
+a *= 2 # a = a*2
+a /= 2 # a = a/2
+a += 2 # a = a+2
+#print(a)
+
+#for n in range(0,10):
+#    print(n)
+
+#while n == 9:
+ #   print(n)
+
+a = 0
+while a != 9:
+    a += 1 # a = a+1
+    print(a)
